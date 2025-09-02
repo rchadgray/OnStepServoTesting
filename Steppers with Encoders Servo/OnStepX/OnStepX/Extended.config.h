87e415ba2fbe79142e7a84199448e4aa5f51bf08
Chad Gray <rchadgray@gmail.com>
2025-09-02 15:26:24 -0400

    bump

--- Steppers with Encoders Servo/OnStepX/OnStepX/Extended.config.h
+++ Steppers with Encoders Servo/OnStepX/OnStepX/Extended.config.h
@@ -18,10 +18,10 @@
 // DEBUG ---------------------------------------------------------------------------------------------------------------------------
 // Enable additional debugging and/or status messages on the specified SERIAL_DEBUG port
 // Note that the SERIAL_DEBUG port cannot be used for normal communication with OnStep
-#define DEBUG                          ON //    OFF, Use ON for background error messages only, use VERBOSE for all           Infreq
+#define DEBUG                         OFF //    OFF, Use ON for background error messages only, use VERBOSE for all           Infreq
                                           //         error and status messages, use CONSOLE for VT100 debug console,
                                           //         or use PROFILER for VT100 task profiler.
-#define DEBUG_SERVO                     1 //    OFF, n. Where n=1 to 9 as the designated axis for logging servo activity.     Option
+#define DEBUG_SERVO                   OFF //    OFF, n. Where n=1 to 9 as the designated axis for logging servo activity.     Option
 #define DEBUG_ECHO_COMMANDS           OFF //    OFF, Use ON or ERRORS_ONLY to log commands to the debug serial port.          Option
 #define SERIAL_DEBUG               Serial // Serial, Use any available h/w serial port. Serial1 or Serial2, etc.              Option
 #define SERIAL_DEBUG_BAUD            9600 //   9600, n. Where n=9600,19200,57600,115200 (common baud rates.)                  Option
