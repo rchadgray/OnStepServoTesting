a7b9fc5ad08eb7f926be2dace9e7e57e311f4346
Chad Gray <rchadgray@gmail.com>
2025-09-03 07:50:58 -0400

    bump

--- Stepper With Encoders Servo 8-31-25/OnStepX-10.26a/OnStepX/Config.h
+++ Stepper With Encoders Servo 8-31-25/OnStepX-10.26a/OnStepX/Config.h
@@ -32,7 +32,7 @@
 #define WEB_SERVER                     ON //    OFF, ON enables Webserver (for Website plugin)
 #define STA_ENABLED                  true //       false, Wifi Station Enabled.                                               Adjust
 #define STA_SSID               "grayWiFi" //      "Home", Wifi Station SSID to connnect to.                                   Adjust
-#define STA_PASSWORD      "rcgrayWiFi216" //  "password", Wifi Station mode password.                                         Adjust
+#define STA_PASSWORD      "" //  "password", Wifi Station mode password.                                         Adjust
 #define STA_DHCP_ENABLED            false //       false, Wifi Station/Ethernet DHCP Enabled.                                 Adjust
 #define STA_IP_ADDR          {10,11,0,55} // ..168,1,55}, Wifi Station/Ethernet IP Address.                                   Adjust
 #define STA_GW_ADDR           {10,11,0,1} // ..,168,1,1}, Wifi Station/Ethernet GATEWAY Address.                              Adjust
